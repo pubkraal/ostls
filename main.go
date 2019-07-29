b74f11796f5f114c3f2f089774c1b20290f2874f
John Kraal <git@johnkraal.com>
2019-07-29 16:48:20 +0200

    Flag parsin and TLS listening

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -13,6 +15,26 @@ func stub(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 }
 
 func main() {
+	cert := flag.String("cert", "./cert.pem", "valid TLS certificate for hosting https")
+	key := flag.String("key", "./key.pem", "valid TLS Private key for hosting https")
+	port := flag.Int("port", 0, "the port to host on. Leave empty to default to 80 or 443 depending on TLS config")
+	host := flag.String("host", "", "the host to listen on, leave empty for all interfaces.")
+	flag.Parse()
+
+	if *port < 0 || *port > 65535 {
+		log.Fatalf("Invalid port number: %d\n", *port)
+	}
+
+	tlsReady := true
+	if _, err := os.Stat(*cert); err != nil {
+		log.Println("Invalid certificate", err)
+		tlsReady = false
+	}
+	if _, err := os.Stat(*key); err != nil {
+		log.Println("Invalid private key", err)
+		tlsReady = false
+	}
+
 	router := httprouter.New()
 
 	// Normal route
@@ -32,5 +54,20 @@ func main() {
 	router.POST("/api/carve-init", stub)
 	router.POST("/api/carve-block", stub)
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	if *port == 0 {
+		if tlsReady {
+			*port = 443
+		} else {
+			*port = 80
+		}
+	}
+
+	hostname := fmt.Sprintf("%s:%d", *host, *port)
+	log.Println("Listening on", hostname)
+
+	if tlsReady {
+		log.Fatal(http.ListenAndServeTLS(hostname, *cert, *key, nil))
+	} else {
+		log.Fatal(http.ListenAndServe(hostname, router))
+	}
 }
